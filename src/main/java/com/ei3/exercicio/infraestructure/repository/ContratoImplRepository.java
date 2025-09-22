b23368cc686be423aa56695da63241113f458753
Gustavo Hillesheim <g.hillesheim@edu.pucrs.br>
2025-09-22 17:02:13 -0300

    Contrato repository criado, inserção de contratos criada, falta testes

---
+++ src/main/java/com/ei3/exercicio/infraestructure/repository/ContratoImplRepository.java
@@ -0,0 +1,44 @@
+package com.ei3.exercicio.infraestructure.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.ei3.exercicio.infraestructure.entity.Contrato;
+import com.ei3.exercicio.infraestructure.repository.interfaces.ContratoRepository;
+import com.ei3.exercicio.infraestructure.repository.interfacesJPA.ContratoRepositoryJPA;
+
+@Repository
+public class ContratoImplRepository implements ContratoRepository {
+
+    @Autowired
+    private ContratoRepositoryJPA contratoRepo; 
+
+    public ContratoImplRepository(){}
+
+    @Override
+    public void insert(Contrato contrato){
+        this.contratoRepo.save(contrato);
+    }
+
+    @Override
+    public List<Contrato> getAll(){
+        return this.contratoRepo.findAll().stream().toList();
+    }
+
+    @Override
+    public Optional<Contrato> getById(long id){
+        return this.contratoRepo.findById(id);
+    }
+
+    @Override
+    public List<Contrato> getAllByPessoaId(long pessoaId) {
+        return this.contratoRepo.findAllByPessoaId(pessoaId);
+    }
+
+
+
+    
+}
