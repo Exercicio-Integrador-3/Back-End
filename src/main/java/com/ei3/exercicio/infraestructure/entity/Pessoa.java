ddc7d88e8bbcd6f2d2415a13676352bf2e3349b8
Marina Silveira Acosta <marinaacosta63@hotmail.com>
2025-09-22 14:40:27 -0300

    Creating Entity Pessoa

---
+++ src/main/java/com/ei3/exercicio/infraestructure/entity/Pessoa.java
@@ -0,0 +1,33 @@
+package com.ei3.exercicio.infraestructure.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+public class Pessoa {
+
+    @Id
+    private long id;
+
+    @NotBlank(message = "O nome não pode estar em branco")
+    @Size(min=2, max=75, message="O nome tem que ter entre 2 e 50 caracteres")
+    private String nome;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
