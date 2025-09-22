e6cb5b2738a38d56807fac752a3d0f0539290ecd
Marina Silveira Acosta <marinaacosta63@hotmail.com>
2025-09-22 15:02:06 -0300

    Adding entity projeto

---
+++ src/main/java/com/ei3/exercicio/infraestructure/entity/Projeto.java
@@ -0,0 +1,80 @@
+package com.ei3.exercicio.infraestructure.entity;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+public class Projeto {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    
+    @NotBlank(message = "O nome não pode estar em branco")
+    @Size(min=2, max=75, message="O nome tem que ter entre 2 e 50 caracteres")
+    private String nome;
+
+    @NotNull
+    private LocalDate dataInicio;
+
+    @NotNull
+    private LocalDate dataFim;
+
+    @NotBlank(message = "A descrição não pode estar em branco")
+    @Size(min=2, max=255, message="A descrição tem que ter entre 2 e 255 caracteres")
+    private String descricao;
+
+    
+    public Projeto() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public LocalDate getDataInicio() {
+        return dataInicio;
+    }
+
+    public void setDataInicio(LocalDate dataInicio) {
+        this.dataInicio = dataInicio;
+    }
+
+    public LocalDate getDataFim() {
+        return dataFim;
+    }
+
+    public void setDataFim(LocalDate dataFim) {
+        this.dataFim = dataFim;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    
+    
+
+}
