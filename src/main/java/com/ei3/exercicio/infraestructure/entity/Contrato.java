8f3102c1a8328a3b503e88a4fb458f03156e3b66
Nicholas Ástor <nicholas.barbosa@edu.pucrs.br>
2025-09-22 15:34:52 -0300

    Creating contrato entity (#31)

---
+++ src/main/java/com/ei3/exercicio/infraestructure/entity/Contrato.java
@@ -0,0 +1,97 @@
+package com.ei3.exercicio.infraestructure.entity;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+public class Contrato {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public long Id;
+
+    @ManyToOne
+    // @JoinColumn(name = "pessoa_id", nullable = false)
+    private Pessoa pessoa;
+
+    @ManyToOne
+    // @JoinColumn(name = "perfil_id", nullable = false)
+    private Perfil perfil;
+
+    @NotNull
+    private LocalDate dataInicio;
+    
+    private LocalDate dataFim;
+
+    @NotNull
+    @Size(max = 40)
+    private int horasSemanais;
+
+    @NotNull
+    private double salarioHora;
+
+    public long getId() {
+        return Id;
+    }
+
+    public void setId(long id) {
+        Id = id;
+    }
+
+    public Pessoa getPessoa() {
+        return pessoa;
+    }
+
+    public void setPessoa(Pessoa pessoa) {
+        this.pessoa = pessoa;
+    }
+
+    public Perfil getPerfil() {
+        return perfil;
+    }
+
+    public void setPerfil(Perfil perfil) {
+        this.perfil = perfil;
+    }
+
+    public LocalDate getDataInicio() {
+        return dataInicio;
+    }
+
+    public void setDataInicio(LocalDate dataInicio) {
+        this.dataInicio = dataInicio;
+    }
+
+    public LocalDate getDataFim() {
+        return dataFim;
+    }
+
+    public void setDataFim(LocalDate dataFim) {
+        this.dataFim = dataFim;
+    }
+
+    public int getHorasSemanais() {
+        return horasSemanais;
+    }
+
+    public void setHorasSemanais(int horasSemanais) {
+        this.horasSemanais = horasSemanais;
+    }
+
+    public double getSalarioHora() {
+        return salarioHora;
+    }
+
+    public void setSalarioHora(double salarioHora) {
+        this.salarioHora = salarioHora;
+    }
+
+    public Contrato(){}
+}
