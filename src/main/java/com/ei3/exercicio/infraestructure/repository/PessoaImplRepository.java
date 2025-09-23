74ab4cddd0c58e104c26dc4a2e1af98569e77daa
Marina Silveira Acosta <marinaacosta63@hotmail.com>
2025-09-23 11:40:01 -0300

    Creating Pessoa Repository

---
+++ src/main/java/com/ei3/exercicio/infraestructure/repository/PessoaImplRepository.java
@@ -0,0 +1,35 @@
+package com.ei3.exercicio.infraestructure.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.ei3.exercicio.infraestructure.entity.Pessoa;
+import com.ei3.exercicio.infraestructure.repository.interfaces.PessoaRepository;
+import com.ei3.exercicio.infraestructure.repository.interfacesJPA.PessoaRepositoryJPA;
+
+@Repository
+public class PessoaImplRepository implements PessoaRepository {
+
+    @Autowired
+    private PessoaRepositoryJPA pessoaRepo;
+
+    public PessoaImplRepository(){}
+
+    @Override
+    public void insert(Pessoa pessoa){
+        this.pessoaRepo.save(pessoa);
+    }
+
+    @Override
+    public List<Pessoa> getAll(){
+        return this.pessoaRepo.findAll();
+    }
+
+    @Override
+    public Optional<Pessoa> getById(long id){
+        return this.pessoaRepo.findById(id);
+    }
+}
---
+++ src/main/java/com/ei3/exercicio/infraestructure/repository/interfaces/PessoaRepository.java
@@ -0,0 +1,12 @@
+package com.ei3.exercicio.infraestructure.repository.interfaces;
+
+import com.ei3.exercicio.infraestructure.entity.Pessoa;
+import java.util.List;
+import java.util.Optional;
+
+public interface PessoaRepository {
+
+    void insert(Pessoa pessoa);
+    List<Pessoa> getAll();
+    Optional<Pessoa> getById(long id);
+}
