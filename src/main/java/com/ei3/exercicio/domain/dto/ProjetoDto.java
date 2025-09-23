1030447aba0a19392b77bed42aa1b31b2445752b
Nicholas Ástor <nicholas.barbosa@edu.pucrs.br>
2025-09-23 11:30:10 -0300

    id not capitalized (#40)

--- src/main/java/com/ei3/exercicio/domain/dto/ProjetoDto.java
+++ src/main/java/com/ei3/exercicio/domain/dto/ProjetoDto.java
@@ -2,4 +2,4 @@ package com.ei3.exercicio.domain.dto;
 
 import java.time.LocalDate;
 
-public record ProjetoDto(long Id, String nome, LocalDate dataInicio, LocalDate dataFim, String descricao) {}
+public record ProjetoDto(long id, String nome, LocalDate dataInicio, LocalDate dataFim, String descricao) {}
