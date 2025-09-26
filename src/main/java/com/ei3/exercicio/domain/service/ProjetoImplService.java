472fb2799a4e68db829db86cf034cfcf1ab53689
Gustavo Hillesheim <g.hillesheim@edu.pucrs.br>
2025-09-26 14:19:19 -0300

    fix status projeto

--- src/main/java/com/ei3/exercicio/domain/service/ProjetoImplService.java
+++ src/main/java/com/ei3/exercicio/domain/service/ProjetoImplService.java
@@ -1,5 +1,6 @@
 package com.ei3.exercicio.domain.service;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -9,13 +10,19 @@ import org.springframework.stereotype.Service;
 import com.ei3.exercicio.domain.dto.CreateProjetoDto;
 import com.ei3.exercicio.domain.dto.ProjetoDto;
 import com.ei3.exercicio.domain.service.interfaces.ProjetoService;
+import com.ei3.exercicio.infraestructure.entity.Alocacao;
 import com.ei3.exercicio.infraestructure.entity.Projeto;
+import com.ei3.exercicio.infraestructure.entity.TipoPerfil;
+import com.ei3.exercicio.infraestructure.repository.interfaces.AlocacaoRepository;
 import com.ei3.exercicio.infraestructure.repository.interfaces.ProjetoRepository;
 
 @Service
 public class ProjetoImplService implements ProjetoService{
     @Autowired
     private ProjetoRepository projetoRepository;
+
+    @Autowired
+    private AlocacaoRepository alocacaoRepository;
 
     public ProjetoImplService(){}
 
@@ -29,6 +36,15 @@ public class ProjetoImplService implements ProjetoService{
     }
 
     public List<ProjetoDto> getAllProjetos(){
+        var response = this.projetoRepository.getAll();
+        for(Projeto p : response){
+            var pps = this.alocacaoRepository.findByProjetoId(p.getId());
+            if(estaAtivo(pps) && dataCorreta(p)){
+                p.setIs_active(true);
+                this.projetoRepository.updateStatus(p);
+            }
+        }
+        
         return this.projetoRepository.getAll()
         .stream()
         .map(ProjetoDto::fromModel)
@@ -46,5 +62,15 @@ public class ProjetoImplService implements ProjetoService{
     public Optional<ProjetoDto> getProjetoById(long id){
         return this.projetoRepository.getById(id)
         .map(ProjetoDto::fromModel);
+    }
+
+    private boolean estaAtivo(List<Alocacao> alocPorProjeto){
+        List<TipoPerfil> allTipoPerfis = List.of(TipoPerfil.GERENTE, TipoPerfil.DEV, TipoPerfil.QA);
+        List<TipoPerfil> listaToTipoPerfil = alocPorProjeto.stream().map(al -> al.getPerfilPessoa().getPerfil().getTipo()).toList();
+        return listaToTipoPerfil.containsAll(allTipoPerfis);
+    }
+
+    private boolean dataCorreta(Projeto p){
+        return (p.getDataInicio().isBefore(LocalDate.now()) || p.getDataInicio().isEqual(LocalDate.now())) && p.getDataFim().isAfter(LocalDate.now());
     }
 }
--- src/test/java/com/ei3/exercicio/services/ProjetoServiceTest.java
+++ src/test/java/com/ei3/exercicio/services/ProjetoServiceTest.java
@@ -1,14 +1,14 @@
 package com.ei3.exercicio.services;
 
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import java.time.LocalDate;
 
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
 
 import com.ei3.exercicio.domain.dto.CreateProjetoDto;
 import com.ei3.exercicio.domain.service.ProjetoImplService;
@@ -18,6 +18,7 @@ import jakarta.transaction.Transactional;
 @SpringBootTest
 @ActiveProfiles("test")
 @Transactional
+@Sql("/test-projectservice-alocacoes.sql")
 public class ProjetoServiceTest {
     
     @Autowired
@@ -47,5 +48,14 @@ public class ProjetoServiceTest {
         assertNotEquals(true, projetoService.getProjetoById(1L));
     }
 
-    
+    @Test
+    public void getAllProjectsOneShouldBeActive(){
+
+        var resposta = this.projetoService.getAllProjetos();
+
+
+
+        boolean actual = resposta.stream().anyMatch(r-> r.is_active());
+        assertTrue(actual);
+    }
 }
