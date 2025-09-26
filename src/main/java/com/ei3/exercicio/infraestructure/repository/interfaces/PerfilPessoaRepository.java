bb8d60db4d98edecd329b569c0f598c32f33d23d
Gustavo Hillesheim <g.hillesheim@edu.pucrs.br>
2025-09-25 23:39:19 -0300

    implementar testes

--- src/main/java/com/ei3/exercicio/infraestructure/repository/interfaces/PerfilPessoaRepository.java
+++ src/main/java/com/ei3/exercicio/infraestructure/repository/interfaces/PerfilPessoaRepository.java
@@ -1,11 +1,15 @@
 package com.ei3.exercicio.infraestructure.repository.interfaces;
 
+import java.util.List;
 import java.util.Optional;
 
 import com.ei3.exercicio.infraestructure.entity.PerfilPessoa;
+import com.ei3.exercicio.infraestructure.entity.TipoPerfil;
 
 public interface PerfilPessoaRepository {
     Optional<PerfilPessoa> findByPessoaId(long pessoaId);
+    Optional<PerfilPessoa> findByPessoaIdAndTipoPerfil(long pessoaId, TipoPerfil tipoPerfil);
     Optional<PerfilPessoa> findById(long perfilPessoaId);
     PerfilPessoa insert(PerfilPessoa perfilPessoa);
+    List<PerfilPessoa> findAllByPessoaId(long pessoaId);
 }
