c8156274eb6014cbd4453ec6e5fa3330ac1230d9
Gustavo Teixeira <gustavohteixeira2904@gmail.com>
2025-09-26 00:17:50 -0300

    testes de alocacao prontos

--- src/test/java/com/ei3/exercicio/services/AlocacaoServiceTest.java
+++ src/test/java/com/ei3/exercicio/services/AlocacaoServiceTest.java
@@ -10,7 +10,6 @@ import java.util.ArrayList;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.cglib.core.Local;
 import org.springframework.test.context.ActiveProfiles;
 
 import com.ei3.exercicio.domain.dto.CreateAlocacaoDto;
@@ -44,12 +43,32 @@ public class AlocacaoServiceTest {
         assertTrue(this.alocacaoService.createAlocacao(alocacao));
     }
 
-    // @Test
-    // public void addingInvalidEntityShouldReturnFalse(){
-    //     var alocacao = new CreateAlocacaoDto(23478, 1, 1, 200);
+    @Test
+    public void addingInvalidEntityShouldReturnFalse(){
+        var idsPessoas = new ArrayList<Long>();
+        idsPessoas.add(23198L);
 
-    //     assertFalse(this.alocacaoService.createAlocacao(alocacao));
-    // }
+        var idsPerfis = new ArrayList<Long>();
+        idsPerfis.add(1L);
+
+        var alocacao = new CreateAlocacaoDto(idsPessoas, idsPerfis, 1, 200);
+
+        assertFalse(this.alocacaoService.createAlocacao(alocacao));
+    }
+
+    
+    @Test
+    public void addingInvalidPerfilShouldReturnFalse(){
+        var idsPessoas = new ArrayList<Long>();
+        idsPessoas.add(1L);
+
+        var idsPerfis = new ArrayList<Long>();
+        idsPerfis.add(1321L);
+
+        var alocacao = new CreateAlocacaoDto(idsPessoas, idsPerfis, 1, 200);
+
+        assertFalse(this.alocacaoService.createAlocacao(alocacao));
+    }
 
     @Test
     public void addingPersonWhoIsAlreadyAllocatedWithDifferentProfilesReturnFalse(){
@@ -66,68 +85,109 @@ public class AlocacaoServiceTest {
         assertFalse(this.alocacaoService.createAlocacao(alocacao1));
     }
 
-    // @Test
-    // public void addingPersonWhoIsAlreadyAllocatedWithSameProfileReturnFalse(){
-    //     var alocacao1 = new CreateAlocacaoDto(1, 1, 1, 200);
-    //     var alocacao2 = new CreateAlocacaoDto(1, 1, 1, 200);
+    @Test
+    public void addingPersonWhoIsAlreadyAllocatedWithSameProfileReturnFalse(){
+        var idsPessoas = new ArrayList<Long>();
+        idsPessoas.add(1L);
 
-    //     this.alocacaoService.createAlocacao(alocacao1);
-    //     assertFalse(this.alocacaoService.createAlocacao(alocacao2));
-    // }
+        var idsPerfis = new ArrayList<Long>();
+        idsPerfis.add(1L);
+        idsPerfis.add(2L);
 
-    // @Test
-    // public void addingTwoPersonsWithDifferentProfilesReturnTrue(){
-    //     var alocacao1 = new CreateAlocacaoDto(1, 1, 1, 200);
-    //     var alocacao2 = new CreateAlocacaoDto(2, 2, 1, 200);
+        var alocacao1 = new CreateAlocacaoDto(idsPessoas, idsPerfis, 1, 200);
 
-    //     this.alocacaoService.createAlocacao(alocacao1);
-    //     assertTrue(this.alocacaoService.createAlocacao(alocacao2));
-    // }
 
-    // @Test
-    // public void addingTwoPersonsWithSameProfileNotBeingGerenteReturnTrue(){
-    //     var alocacao1 = new CreateAlocacaoDto(1, 2, 1, 200);
-    //     var alocacao2 = new CreateAlocacaoDto(2, 2, 1, 200);
+        this.alocacaoService.createAlocacao(alocacao1);
+        assertFalse(this.alocacaoService.createAlocacao(alocacao1));
+    }
 
-    //     this.alocacaoService.createAlocacao(alocacao1);
-    //     assertTrue(this.alocacaoService.createAlocacao(alocacao2));  
-    // }
+    @Test
+    public void addingTwoPersonsWithSameProfileNotBeingGerenteReturnTrue(){
 
-    // @Test
-    // public void addingTwoPersonsWithSameProfileBeingGerenteReturnFalse(){
-    //     var alocacao1 = new CreateAlocacaoDto(1, 1, 1, 200);
-    //     var alocacao2 = new CreateAlocacaoDto(2, 1, 1, 200);
+        var idsPessoas = new ArrayList<Long>();
+        idsPessoas.add(1L);
+        idsPessoas.add(2L);
 
-    //     this.alocacaoService.createAlocacao(alocacao1);
-    //     assertFalse(this.alocacaoService.createAlocacao(alocacao2));
-    // }
 
-    // @Test
-    // public void totalCostOfAProject(){
-    //     this.alocacaoService.createAlocacao(new CreateAlocacaoDto(1, 1, 1, 120));
+        var idsPerfis = new ArrayList<Long>();
+        idsPerfis.add(2L);
+        idsPerfis.add(2L);
 
-    //     assertEquals(8400.000, this.alocacaoService.custoTotal(1L));
-    // } 
+        var alocacao1 = new CreateAlocacaoDto(idsPessoas, idsPerfis, 1, 200);
 
-    // @Test
-    // public void costByPeriodOfTime(){      
-    //     this.alocacaoService.createAlocacao(new CreateAlocacaoDto(1, 1, 1, 120));
+        assertTrue(this.alocacaoService.createAlocacao(alocacao1));
+    }
 
-    //     assertEquals(3360, this.alocacaoService.custoPeriodo(1,LocalDate.of(2025, 9, 24) , LocalDate.of(2025, 10, 1)));
-    // }
+    @Test
+    public void addingTwoPersonsWithSameProfileBeingGerenteReturnFalse(){
+        var idsPessoas = new ArrayList<Long>();
+        idsPessoas.add(1L);
+        idsPessoas.add(2L);
 
-    // @Test
-    // public void costByPeriodOfTimeOutOfTheTimeOfTheProject(){
-    //     this.alocacaoService.createAlocacao(new CreateAlocacaoDto(1, 1, 1, 120));
 
-    //     assertEquals(0, this.alocacaoService.custoPeriodo(1, LocalDate.of(2025, 10, 31), LocalDate.of(2025, 11, 5)));
-    // }
+        var idsPerfis = new ArrayList<Long>();
+        idsPerfis.add(1L);
+        idsPerfis.add(1L);
 
-    // @Test
-    // public void costByPeriodOfTimeBeforeTheTimeOfTheProject(){
-    //     this.alocacaoService.createAlocacao(new CreateAlocacaoDto(1, 1, 1, 120));
+        var alocacao1 = new CreateAlocacaoDto(idsPessoas, idsPerfis, 1, 200);
+        assertFalse(this.alocacaoService.createAlocacao(alocacao1));
 
-    //     assertEquals(0, this.alocacaoService.custoPeriodo(1, LocalDate.of(2024, 10, 31), LocalDate.of(2024, 11, 5)));
-    // }
+
+    }
+
+    @Test
+    public void totalCostOfAProject(){
+        var idsPessoas = new ArrayList<Long>();
+        idsPessoas.add(1L);
+
+
+        var idsPerfis = new ArrayList<Long>();
+        idsPerfis.add(1L);
+
+        this.alocacaoService.createAlocacao(new CreateAlocacaoDto(idsPessoas, idsPerfis, 1, 120));
+
+        assertEquals(8400.000, this.alocacaoService.custoTotal(1L));
+    } 
+
+    @Test
+    public void costByPeriodOfTime(){      
+        var idsPessoas = new ArrayList<Long>();
+        idsPessoas.add(1L);
+
+
+        var idsPerfis = new ArrayList<Long>();
+        idsPerfis.add(1L);
+
+        this.alocacaoService.createAlocacao(new CreateAlocacaoDto(idsPessoas, idsPerfis, 1, 120));
+        assertEquals(3360, this.alocacaoService.custoPeriodo(1,LocalDate.of(2025, 9, 24) , LocalDate.of(2025, 10, 1)));
+    }
+
+    @Test
+    public void costByPeriodOfTimeOutOfTheTimeOfTheProject(){
+        var idsPessoas = new ArrayList<Long>();
+        idsPessoas.add(1L);
+
+
+        var idsPerfis = new ArrayList<Long>();
+        idsPerfis.add(1L);
+
+        this.alocacaoService.createAlocacao(new CreateAlocacaoDto(idsPessoas, idsPerfis, 1, 120));
+
+        assertEquals(0, this.alocacaoService.custoPeriodo(1, LocalDate.of(2025, 10, 31), LocalDate.of(2025, 11, 5)));
+    }
+
+    @Test
+    public void costByPeriodOfTimeBeforeTheTimeOfTheProject(){
+        var idsPessoas = new ArrayList<Long>();
+        idsPessoas.add(1L);
+
+
+        var idsPerfis = new ArrayList<Long>();
+        idsPerfis.add(1L);
+
+        this.alocacaoService.createAlocacao(new CreateAlocacaoDto(idsPessoas, idsPerfis, 1, 120));
+
+        assertEquals(0, this.alocacaoService.custoPeriodo(1, LocalDate.of(2024, 10, 31), LocalDate.of(2024, 11, 5)));
+    }
 
 }
